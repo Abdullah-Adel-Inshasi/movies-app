f94727268ec2080ba51172e6554d9a4547a01223
Abdullah Adel <abdullahadel@kiitos-tech.com>
2022-11-11 10:53:37 +0200

    _app cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,18 @@
-import { useEffect } from "react";
 import { NextPage } from "next";
-import { ReactElement, ReactNode, useMemo, useState } from "react";
+import { ReactElement, ReactNode } from "react";
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
 import LayoutProvider from "../providers/Layout";
-import { IUserContextValue, userContext } from "../providers/UserProvider";
 import "styles/globals.css";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const [user, setUser] = useState<IUserContextValue["user"]>(null);
-  // const router = useRouter();
-
-  const values = useMemo(() => ({ user, setUser }), [user, setUser]);
-  // useEffect(() => {
-  //   console.log(values.user);
-  // }, [values.user]);
   if (Component.getLayout !== undefined) {
-    return (
-      <userContext.Provider value={values}>
-        <Component {...pageProps} />
-      </userContext.Provider>
-    );
+    return <Component {...pageProps} />;
   }
 
   return (
     <LayoutProvider>
-      <userContext.Provider value={values}>
-        <Component {...pageProps} />
-      </userContext.Provider>
+      <Component {...pageProps} />
     </LayoutProvider>
   );
 }
