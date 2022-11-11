5d4218de6bbfecfcbbe6e1ce3ef14cbe46606616
Abdullah Adel <abdullahadel@kiitos-tech.com>
2022-11-11 10:50:18 +0200

    Added Icons

---
+++ src/Icons/index.ts
@@ -0,0 +1,16 @@
+// export NotificationsBell from "./bell";
+export { default as InfoIcon } from "./info";
+export { default as MagnifyingGlass } from "./magnifying-glass";
+export { default as PlayIcon } from "./triangle";
+export { default as NotificationsBell } from "./bell";
+export { default as LockIcon } from "./LockIcon";
+export { default as StarIcon } from "./star";
+export { default as MoneyIcon } from "./money";
+export { default as PlusIcon } from "./PlusIcon";
+
+// bookmark icons
+// export {default as } from ''
+export { default as HollowBookmarkIcon } from "./BookmarkIcons/RemoveBookmark/HollowBookmarkIcon";
+export { default as FilledRemoveBookmarkIcon } from "./BookmarkIcons/RemoveBookmark/FilledRemoveBookmarkIcon";
+export { default as HollowBookmark } from "./BookmarkIcons/BookmarkIcon/HollowBookmark";
+export { default as FilledBookmark } from "./BookmarkIcons/BookmarkIcon/FilledBookmark";
