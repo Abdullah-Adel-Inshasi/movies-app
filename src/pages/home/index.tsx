8cb81c63eb6ed583826f778893854d641968119f
Abdullah Adel <abdullahadel@kiitos-tech.com>
2022-11-14 11:53:30 +0200

    Added movies search functionality

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,17 +3,56 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { getPopularMovies } from "@API/requests";
 import { IPopularMoviesResponse } from "@API/types";
 import { HeroImage, MediaSection } from "@Components";
+import { useEffect, useState } from "react";
+import useDebounce from "~/src/hooks/useDebounce";
+import searchMovieByName from "~/lib/api/requests/searchMovies";
+import { IMovie } from "~/lib/api/types/IPopularMoviesResponse";
+import LoadingMediaSection from "~/src/Components/MediaSection/Loading";
 const Home = ({ movies }: { movies: IPopularMoviesResponse["results"] }) => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [results, setResults] = useState<IMovie[]>(movies);
+  const [isSearching, setIsSearching] = useState<boolean>(false);
+
+  const debouncedSearchTerm: string = useDebounce<string>(searchTerm, 1000);
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      setIsSearching(true);
+      searchMovieByName(debouncedSearchTerm)
+        .then((data) => {
+          if (data.results.length) {
+            setResults(data.results);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => setIsSearching(false));
+    } else {
+      setResults(movies);
+    }
+  }, [debouncedSearchTerm, movies]);
   return (
     <div className=" bg-background">
       <div className="min-screen-min">
         <HeroImage
-          image={movies[2].backdrop_path}
-          title={movies[2].title}
-          description={movies[2].overview}
-          genres={movies[2].genres}
+          image={results[0].backdrop_path}
+          title={results[0].title}
+          description={results[0].overview}
+          genres={results[0].genres}
         />
-        <MediaSection title={"Popular Movies"} movies={movies} />
+        <input
+          placeholder="Search movies"
+          className="border border-blue-400 focus:outline-red-500"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        {isSearching ? (
+          <LoadingMediaSection searchQuery={debouncedSearchTerm} />
+        ) : (
+          <MediaSection
+            title={searchTerm ? `Search for "${searchTerm}"` : "Popular Movies"}
+            movies={results}
+          />
+        )}
       </div>
     </div>
   );
@@ -26,7 +65,7 @@ export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   return {
     props: {
       ...(await serverSideTranslations(locale!, ["common"])),
-      movies: movies.slice(0, 6),
+      movies: movies,
     },
   };
 };
