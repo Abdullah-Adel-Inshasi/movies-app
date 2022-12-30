ad6f5c1d648b12244e4b0e694549532d9f2b33dd
Abdullah Adel <abdullah.adel.inshasi@gmail.com>
2022-12-30 16:40:55 +0200

    added search functionality

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,66 +1,84 @@
-import { GetServerSideProps } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { getPopularMovies } from "@API/requests";
-import { IPopularMoviesResponse } from "@API/types";
-import { HeroImage, MediaSection } from "@Components";
 import { useEffect, useState } from "react";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { getPopularMovies, searchMoviesByName } from "@API/requests";
+import { HeroImage, MediaSection, LoadingMediaSection } from "@Components";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import useDebounce from "~/src/hooks/useDebounce";
-import searchMovieByName from "~/lib/api/requests/searchMovies";
-import { IMovie } from "~/lib/api/types/IPopularMoviesResponse";
-import LoadingMediaSection from "~/src/Components/MediaSection/Loading";
+
 const Home = ({ movies }: { movies: IPopularMoviesResponse["results"] }) => {
-  const [searchTerm, setSearchTerm] = useState<string>("");
   const [results, setResults] = useState<IMovie[]>(movies);
   const [isSearching, setIsSearching] = useState<boolean>(false);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const debouncedSearchTerm = useDebounce<string | null>(searchTerm, 1000);
 
-  const debouncedSearchTerm: string = useDebounce<string>(searchTerm, 1000);
+  const router = useRouter();
+  const { search } = router.query;
+
   useEffect(() => {
     if (debouncedSearchTerm) {
       setIsSearching(true);
-      searchMovieByName(debouncedSearchTerm)
+      searchMoviesByName(debouncedSearchTerm)
         .then((data) => {
           if (data.results.length) {
             setResults(data.results);
+            router.replace(`?search=${debouncedSearchTerm}`, undefined, {
+              shallow: true,
+            });
           }
         })
-        .catch((err) => {
-          console.log(err);
-        })
+        .catch((err) => {})
         .finally(() => setIsSearching(false));
     } else {
       setResults(movies);
     }
   }, [debouncedSearchTerm, movies]);
   return (
-    <div className=" bg-background">
-      <div className="min-screen-min">
-        <HeroImage
-          image={results[0].backdrop_path}
-          title={results[0].title}
-          description={results[0].overview}
-          genres={results[0].genres}
+    <>
+      <HeroImage
+        image={results[0].backdrop_path}
+        title={results[0].title}
+        description={results[0].overview}
+        genres={results[0].genres}
+      />
+      <input
+        placeholder="Search movies"
+        className="border border-blue-400 focus:outline-red-500"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {isSearching ? (
+        <LoadingMediaSection searchQuery={debouncedSearchTerm ?? ""} />
+      ) : (
+        <MediaSection
+          title={
+            search || debouncedSearchTerm
+              ? `Search for "${search ?? debouncedSearchTerm}"`
+              : "Popular Movies"
+          }
+          movies={results}
         />
-        <input
-          placeholder="Search movies"
-          className="border border-blue-400 focus:outline-red-500"
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        {isSearching ? (
-          <LoadingMediaSection searchQuery={debouncedSearchTerm} />
-        ) : (
-          <MediaSection
-            title={searchTerm ? `Search for "${searchTerm}"` : "Popular Movies"}
-            movies={results}
-          />
-        )}
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+export const getServerSideProps: GetServerSideProps = async ({
+  locale,
+  query,
+}) => {
+  if (query.search) {
+    const { results: movies } = await searchMoviesByName(
+      query.search.toString()
+    );
+    return {
+      props: {
+        ...(await serverSideTranslations(locale!, ["common"])),
+        movies: movies,
+      },
+    };
+  }
   const { results: movies } = await getPopularMovies();
   return {
     props: {
