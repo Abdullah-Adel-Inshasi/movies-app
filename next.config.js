2e29b5b8cda996b442ed8e5c182ff856bb249dd4
Abdullah Adel <abdullah.adel.inshasi@gmail.com>
2023-01-08 10:24:39 +0200

    remove unused code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require("next-i18next");
-const fs = require("fs");
 
 const nextConfig = {
   reactStrictMode: true,
@@ -11,20 +9,6 @@ const nextConfig = {
   },
   images: {
     domains: ["image.tmdb.org"],
-  },
-  webpack(config) {
-    const srcPath = (subdir) => path.join(__dirname, "src", subdir);
-    const getFilesAndDirectories = (source) =>
-      fs
-        .readdirSync(source, { withFileTypes: true })
-        .map((dirent) => dirent.name);
-    let absoluteImports = {};
-    getFilesAndDirectories("src").forEach((fileName) => {
-      const fileNameWithoutExtension = path.parse(fileName).name;
-      absoluteImports[`@/${fileNameWithoutExtension}`] = srcPath(fileName);
-    });
-    config.resolve.alias = { ...config.resolve.alias, ...absoluteImports };
-    return config;
   },
 };
 
